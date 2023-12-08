5ada2ad6ad2993cb8e69a86d9cc349b30b5dba01
Jackyw225 <zhenjiew@126.com>
2023-12-08 16:26:00 +0800

    try

--- 3.py
+++ 3.py
@@ -1,13 +1,1 @@
-import math
-
-
-def combinations_count(n, r):
-    return math.factorial(n) // (math.factorial(n - r) * math.factorial(r))
-
-user = 51
-sum = 0
-for active in range(11):
-    rate = float(combinations_count(user, active)) * pow(0.1, active) * pow(0.9, user-active)
-    sum = sum + rate
-print(1-sum)
-
+print('try')
